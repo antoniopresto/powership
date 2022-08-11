10fe1f049c9285dee58ee9fb17de7de4d6d3c662
antoniopresto <antoniopresto@gmail.com>
2022-08-10 22:50:41 -0300

    feat(utils): export utils

---
+++ packages/utils/src/index.ts
@@ -0,0 +1,45 @@
+import * as TU from './typeUtils';
+
+export * from './devAssert';
+export * from './DarchJSON';
+export * from './getTypeName';
+export * from './RuntimeError';
+export * from './getKeys';
+export * from './keyBy';
+export * from './groupBy';
+export * from './sortBy';
+export * from './getByPath';
+export * from './isBrowser';
+export * from './filterNull';
+export * from './emailRegex';
+export * from './createProxy';
+export * from './expectedType';
+export * from './uniq';
+export * from './areEqual';
+export * from './env';
+export * from './ensureArray';
+export * from './hashString';
+export * from './delay';
+export * from './textToBase64';
+export * from './pluralize';
+export * from './memoize';
+export * from './scapeRegex';
+export * from './invariant'
+export * from './upperFirst'
+export * from './inspectObject'
+export * from './threadId'
+export * from './hoper'
+export * from './StrictMap'
+export * from './mitt'
+export * from './stringCase'
+export * from './dateSerialize'
+
+// export * from './logLevels'
+// export * from './logger'
+// export * from './conust'
+// export * from './big'
+// export * from './ulid'
+// export * from './jsonToTypescript'
+
+
+export { TU };
