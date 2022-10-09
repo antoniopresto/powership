d31bb014f14d3195957ca3b0b6df1b81100c65fd
antoniopresto <antoniopresto@gmail.com>
2022-10-09 18:58:57 -0300

    feat: limit RuntimeError stack trace

--- packages/utils/src/RuntimeError.ts
+++ packages/utils/src/RuntimeError.ts
@@ -1,18 +1,49 @@
 import { inspectObject } from './inspectObject';
 
+export type RunTimeErrorOptions = {
+  skipStackLines?: number;
+  depth?: number;
+  limitStackTrace?: number;
+};
+
 export class RuntimeError extends Error {
   name = 'RuntimeError';
 
   details;
   detailsString = '';
 
-  constructor(message: string, details: any, skipStackLines = 0, depth = 10) {
+  /**
+   * @deprecated use options via object RunTimeErrorOptions instead
+   * @param message
+   * @param details
+   * @param skipStackLines
+   * @param depth
+   */
+  constructor(
+    message: string,
+    details: any,
+    skipStackLines?: number,
+    depth?: number
+  );
+
+  constructor(message: string, details: any, options?: RunTimeErrorOptions);
+
+  constructor(message: string, details: any, ..._config) {
     super(message);
     this.details = details;
     this.detailsString = message;
 
+    const {
+      skipStackLines = 0,
+      depth = 10,
+      limitStackTrace = 100,
+    } = prepareArgs(_config as any);
+
     if (details) {
-      const detailsString = inspectObject(details, { tabSize: 0, depth });
+      const detailsString = inspectObject(details, { tabSize: 0, depth })
+        //
+        .slice(0, limitStackTrace);
+
       this.detailsString += `\n${detailsString}`;
 
       const stack = (this.stack || '').split('\n');
@@ -27,4 +58,15 @@ export class RuntimeError extends Error {
       this.stack = newStack.join('\n');
     }
   }
+}
+
+function prepareArgs(
+  _config: [RunTimeErrorOptions] | [number | undefined, number | undefined]
+): RunTimeErrorOptions {
+  return typeof _config[0] === 'object'
+    ? _config[0]
+    : {
+        skipStackLines: _config[0],
+        depth: _config[1],
+      };
 }
