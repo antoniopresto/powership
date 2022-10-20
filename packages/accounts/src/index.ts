1baad23ac8466a3cb02729add68d80a2dfc4311d
antoniopresto <antoniopresto@gmail.com>
2022-10-20 09:57:12 -0300

    wip

--- packages/accounts/src/index.ts
+++
@@ -1,9 +0,0 @@
-export * from './entity';
-export * from './AccountSchema';
-export * from './validateUserName';
-export { Token } from './TokenType';
-export { TokenSchema } from './TokenType';
-export { tokenKindEnum } from './TokenType';
-export { AccessType } from './AccessType';
-export { AccessTypeSchema } from './AccessType';
-export { AccessTypeBase } from './AccessType';
