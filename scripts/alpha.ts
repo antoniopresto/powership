5d7fe7287fcbd52de74aaeb3b2db0f81375fef92
antoniopresto <antoniopresto@gmail.com>
2022-12-01 22:56:29 -0300

    alpha-version

--- scripts/alpha.ts
+++ scripts/alpha.ts
@@ -1,8 +1,27 @@
 import { rumm } from './rumm';
 import { ulid } from '@backland/utils';
 
+const version = `0.0.0-alpha.${ulid()}`;
+
 rumm().map(({ saveJSON, json, run }) => {
-  json.version = `0.0.0-alpha.${ulid()}`;
+  json.version = version;
+
+  Object.keys(json.dependencies || {}).forEach((dep) => {
+    if (dep.match(/backland/)) {
+      json.dependencies![dep] = version;
+    }
+  });
+  Object.keys(json.peerDependencies || {}).forEach((dep) => {
+    if (dep.match(/backland/)) {
+      json.peerDependencies![dep] = version;
+    }
+  });
+  Object.keys(json.devDependencies || {}).forEach((dep) => {
+    if (dep.match(/backland/)) {
+      json.devDependencies![dep] = version;
+    }
+  });
   saveJSON();
   run('prettier ./package.json --write');
+  run('npm publish --tag=next');
 });
