63f0d8eb7a7bc77cb480a57d64e6e36f9d4413f7
antoniopresto <antoniopresto@gmail.com>
2022-12-06 20:39:00 -0300

    feat(utils): add dateTokens()

---
+++ packages/utils/src/__tests__/dateTokens.spec.ts
@@ -0,0 +1,21 @@
+import { dateTokens } from '../dateTokens';
+
+describe('dateTokens', () => {
+  // afterEach();
+
+  test('works', async () => {
+    const tokens = dateTokens({ date: '2022-09-25', locale: 'pt-br' });
+
+    expect(tokens).toEqual({
+      DD: '25',
+      HH: '00',
+      MM: '09',
+      MMM: 'set',
+      MMMM: 'setembro',
+      YYYY: '2022',
+      dddd: 'domingo',
+      mm: '00',
+      ss: '00',
+    });
+  });
+});
---
+++ packages/utils/src/dateTokens.ts
@@ -0,0 +1,77 @@
+import dayjs from 'dayjs';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+import esLocale from 'dayjs/locale/es';
+import brLocale from 'dayjs/locale/pt-br';
+
+export const DateLocales = {
+  es: esLocale,
+  ['pt-br']: brLocale,
+  ['pt-BR']: brLocale,
+  ['pt_BR']: brLocale,
+};
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
+
+export { dayjs };
+
+export type DateParsed = ReturnType<typeof dateTokens>;
+
+export type DateInit = string | Date | number;
+
+export type DateJsLocale = Parameters<dayjs.Dayjs['locale']>[0] extends infer R
+  ? R extends object
+    ? R
+    : never
+  : never;
+
+export type LocaleInit = keyof typeof DateLocales | DateJsLocale;
+
+export type FormatDateInit = {
+  date: DateInit;
+  timezone?: string;
+  locale?: LocaleInit;
+  format?: string;
+};
+
+export function dateTokens(init: FormatDateInit | DateInit) {
+  const params: FormatDateInit =
+    typeof init === 'object' && 'date' in init ? init : { date: init };
+
+  const { date: dateInit, format, timezone, locale } = params;
+  const data = [dateInit];
+
+  if (format) {
+    data.push(format);
+  }
+
+  let date: dayjs.Dayjs = dayjs(...data).tz(timezone);
+
+  if (locale) {
+    if (typeof locale === 'string') {
+      date = date.locale(DateLocales[locale]);
+    }
+    if (typeof locale === 'object') {
+      date = date.locale(locale);
+    }
+  }
+
+  const dateString = date.format('YYYY*MMMM*MMM*MM*DD*dddd*HH*mm*ss');
+
+  const [YYYY, MMMM, MMM, MM, DD, dddd, HH, mm, ss] = dateString.split('*');
+
+  return {
+    YYYY,
+    MMMM,
+    MMM,
+    MM,
+    DD,
+    dddd,
+    HH,
+    mm,
+    ss,
+  };
+}
