252de46867bdbc027e2a1c0ee71ee46fabb9447f
antoniopresto <antoniopresto@gmail.com>
2023-01-12 22:42:49 -0300

    alpha version 0.0.0-alpha.20230113014048970

--- packages/entity/src/index.ts
+++ packages/entity/src/index.ts
@@ -2,3 +2,5 @@ export * from './Entity';
 export * from './EntityPlugin';
 export * from './EntityOptions';
 export * from './EntityInterfaces';
+export * from './tsfyEntity/tsfyEntity';
+export * from './tsfyEntity/tsfyEntitiesWriter';
