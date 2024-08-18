4e3db1652c4d4fb7a9ac300b6185b5699b8f97af
macbook <antoniopresto@gmail.com>
2024-08-18 14:36:01 -0300

    externalize deps

--- packages/utils/src/uniq.ts
+++ packages/utils/src/uniq.ts
@@ -1,8 +1,6 @@
 import { uniq as _uniq } from 'lodash';
 import { uniqBy as _uniqBy } from 'lodash';
 
-// preventing direct code dependency from external lib.
-
 export function uniq<T>(array: T[] | null | undefined): T[] {
   return _uniq(array);
 }
